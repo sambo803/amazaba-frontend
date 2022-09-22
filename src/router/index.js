3ae3fd990064cf2b244cdee1e5d4c8cb6a3b4443
samuel <sbacon803@gmail.com>
2022-09-22 09:22:59 -0600

    add index action

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
+import axios from "axios";
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductsIndex from '../views/ProductsIndex.vue'
+
 
 const routes = [
   {
@@ -14,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/products',
+    name: 'products-index',
+    component: ProductsIndex
   }
 ]
 
---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,32 @@
+<script>
+  import axios from "axios";
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Products Page!",
+        products: [],
+      };
+    },
+    created: function () {
+      this.indexProducts();
+    },
+    methods: {
+      indexProducts: function () {
+        axios.get("/products").then((response) => {
+          console.log("getting products index", response);
+          this.products = response.data;
+        });
+      }
+    },
+  };
+</script>
+
+<template>
+  <div class="index">
+    <h1>{{ message }}</h1>
+
+    <h2>{{products}}</h2>
+  </div>
+</template>
+
+<style></style>
